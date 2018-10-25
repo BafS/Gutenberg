d4adb822ce86bc0f36279dc9afc8ed4903a2fc57
BafS <fabacrans@gmail.com>
2018-10-24 23:16:22 -0700

    Update packages and version

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,24 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 
 gulp.task('build-css', () => {
-	return gulp.src('scss/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('dist'));
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', () => {
-	return gulp.src('scss/**/*.scss')
-		.pipe(sass({
-			outputStyle: 'compressed'
-		}).on('error', sass.logError))
-		.pipe(rename({
-            suffix: '.min'
-        }))
-		.pipe(gulp.dest('dist'));
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', () => {
-	gulp.watch('scss/**/*.scss', ['build-css']);
+  gulp.watch('scss/**/*.scss', ['build-css']);
 });
 
 gulp.task('default', ['build-css', 'minify-css']);
